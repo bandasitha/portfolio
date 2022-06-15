6e9eed311681e48a0a577775b2b709c4e67f7fb9
ambz <a.bandasith@gmail.com>
2022-06-15 10:16:53 -0700

    updating firebase keys

--- portfolio-app/src/db.js
+++ portfolio-app/src/db.js
@@ -2,9 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const app = initializeApp({
-  apiKey: "AIzaSyCusxh06uSE6GLpav156KTejDQ9y6w3uGE",
-  authDomain: "portfolio-contact-form-87424.firebaseapp.com",
-  projectId: "portfolio-contact-form-87424",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: "portfolio-contact-form-87424.appspot.com",
   messagingSenderId: "337587494874",
   appId: "1:337587494874:web:145e2dbe7b19b4f024aae3",
